585e110bc4dbe81fd9da4aab5e7fe564d6475644
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-05-02 14:39:06 +0600

    primitive data type added

---
+++ src/primitive.ts
@@ -0,0 +1,16 @@
+
+let userName:string = "Sharafat";
+// userName = 12; is not allowed because it is a string type variable
+
+let userId = 123;
+// userId = "123"; is not allowed because it is a number type variable
+userId.toFixed(2); // toFixed is a method available for number type variables
+console.log(userId);
+
+let isLoggedIn:boolean = true;
+// isLoggedIn = "true"; is not allowed because it is a boolean type variable
+isLoggedIn = false;
+
+
+
+
